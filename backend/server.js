0f492d51bea6f4fe1b2880e559501d829dd33c71
Hiago <hiago.ribeiro105@gmail.com>
2025-08-28 00:08:36 -0300

    end project

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,36 @@ const app = express(); //iniciando a funcao
 
 const PORT = 3000; //definindo uma porta
 
-app.use(express.urlencoded({extended: true})); //Pegando as informações do formulario e deixando de ma forma que o código entenda
+app.use(express.urlencoded({extended: true})); //Pegando as informações do formulario e deixando de forma que o código entenda
 
-app.use(express.static(path.join(__dirname, "..frontend"))); //Dando a requisição para quando for solicitado a página estatica
+app.use(express.static(path.join(__dirname, "../frontend"))); //Dando a requisição para quando for solicitado a página estatica
 
+app.post('/login', (req, res) => { //quando uma requisição do tipo post chegar com a url login...
+    const {email, password} = req.body; //...eu armazeno as informações do formulario de login nas variaveis de mesmo nome
+    const user = users.find(u => u.email === email && u.password === password); //Dentro de uma variavel para armazenar a resposta, verifico se existe algum email e senha dentro de users que seja igual ao que foi enviado pelo form.
+
+    if (user)
+    {
+        res.send
+        (`
+            <script>
+                alert('Login Sucess');
+                window.location.href = '/';
+            </script>
+        `)
+    }
+    else
+    {
+        res.send
+        (`
+            <script>
+                alert('Login Sucess');
+                window.location.href = '/';
+            </script>
+        `)
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
