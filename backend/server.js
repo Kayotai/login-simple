a12d561dba5eedf4bbf9dd09e636693a60769fd8
Hiago <hiago.ribeiro105@gmail.com>
2025-09-04 09:19:23 -0300

    connect database

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,46 @@
 const express = require('express'); //vai ajudar a cuidar da parte do servidor
 const path = require('path'); //vai ajudar a concertar os caminhos de arquivos de acordo com o modelo da máquina
 const users = require('./data'); //importando a lista de usuarios
-
-
+const mongoose = require('mongoose'); //importando mongoose
 const app = express(); //iniciando a funcao
+
+const MONGODB_URI = 'mongodb+srv://troglodita9090:<db_password>@cluster0.o3y6qtb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const PORT = 3000; //definindo uma porta
 
-app.use(express.urlencoded({extended: true})); //Pegando as informações do formulario e deixando de forma que o código entenda
+//#region Application(app)
+    app.use(express.urlencoded({extended: true})); //Pegando as informações do formulario e deixando de forma que o código entenda
+    app.use(express.static(path.join(__dirname, "../frontend"))); //Dando a requisição para quando for solicitado a página estatica
+    app.post('/login', (req, res) => { //quando uma requisição do tipo post chegar com a url login...
+        const {email, password} = req.body; //...eu armazeno as informações do formulario de login nas variaveis de mesmo nome
+        const user = users.find(u => u.email === email && u.password === password); //Dentro de uma variavel para armazenar a resposta, verifico se existe algum email e senha dentro de users que seja igual ao que foi enviado pelo form.
 
-app.use(express.static(path.join(__dirname, "../frontend"))); //Dando a requisição para quando for solicitado a página estatica
+        if (user) //Se existir um user isso ocorre
+        {
+            res.send //Envio a resposta 
+            (`
+                <script>
+                    alert('Login Sucess yep');
+                    window.location.href = '/';
+                </script>
+            `);
+        }
+        else //Do contrario..
+        {
+            res.send
+            (`
+                <script>
+                    alert('Login Error');
+                    window.location.href = '/';
+                </script>
+            `);
+        }
+    });
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando em http://localhost:${PORT}`);
+    }); //Iniciando a porta
+//#endregion
 
-app.post('/login', (req, res) => { //quando uma requisição do tipo post chegar com a url login...
-    const {email, password} = req.body; //...eu armazeno as informações do formulario de login nas variaveis de mesmo nome
-    const user = users.find(u => u.email === email && u.password === password); //Dentro de uma variavel para armazenar a resposta, verifico se existe algum email e senha dentro de users que seja igual ao que foi enviado pelo form.
-
-    if (user)
-    {
-        res.send
-        (`
-            <script>
-                alert('Login Sucess yep');
-                window.location.href = '/';
-            </script>
-        `);
-    }
-    else
-    {
-        res.send
-        (`
-            <script>
-                alert('Login Error');
-                window.location.href = '/';
-            </script>
-        `);
-    }
-});
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+mongoose.connect(MONGODB_URI)
+    .then(() => console.log('Connected'))
+    .catch(err => console.error('Error'));
