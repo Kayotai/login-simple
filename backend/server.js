bf9de76abe650788a555b6a9c72a1c322462d97b
Hiago <hiago.ribeiro105@gmail.com>
2025-09-10 22:34:57 -0300

    update security

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ const express = require('express'); //vai ajudar a cuidar da parte do servidor
 const path = require('path'); //vai ajudar a concertar os caminhos de arquivos de acordo com o modelo da máquina
 const User = require('./user'); //importando a lista de usuarios
 const mongoose = require('mongoose'); //importando mongoose
+const bcrypt = require('bcrypt'); //codificação das senhas
+
 const app = express(); //iniciando a funcao
 
 const MONGODB_URI = process.env.MONGO_URI;
@@ -17,12 +19,22 @@ mongoose.connect(MONGODB_URI)
     app.use(express.urlencoded({extended: true})); //Pegando as informações do formulario e deixando de forma que o código entenda
     app.use(express.static(path.join(__dirname, "../frontend"))); //Dando a requisição para quando for solicitado a página estatica
     
-    app.post('/new', async (req, res) => { //Se uma requisição new chegar, informo que vai ser uma função que demanda tempo...
+    app.post('/signup', async (req, res) => { //Se uma requisição new chegar, informo que vai ser uma função que demanda tempo...
         
         try //tentativa...
         {
             const {email, password} = req.body; //Pego as informações vindas da requisição do front
-            const newUser = new User({email, password}); //Crio um novo cadastro
+            
+            // Adição da validação de back-end
+            if (!email || !password) 
+            {
+                return res.status(400).send('Email e senha são obrigatórios.');
+            };
+
+            //gerando uma string aleatória
+            const saltRounds = 10;
+            const hashedPassword = await bcrypt.hash(password, saltRounds); //codificando a senha
+            const newUser = new User({email, password: hashedPassword}); //Crio um novo cadastro
             await newUser.save(); //Pausa da execução aqui
             res.send('Usuário cadastrado com sucesso');
         }
@@ -38,15 +50,24 @@ mongoose.connect(MONGODB_URI)
         try //Tentativa
         {
             const {emai, password} = req.body; //pego as informações do front
-            const user = await User.findOne({email}); //digo para esperar aqui enquanto é verificado se o email existe no bancode dados
+            
+             // Adição da validação de back-end
+            if (!email || !password) 
+            {
+                return res.status(400).send('Email e senha são obrigatórios.');
+            };
+            
+            const user = await User.findOne({email}); //digo para esperar aqui enquanto  puxo as informações da conta e armazeno no user
             if (!user) //Se o usuario não for encontrado
             {
-                return res.status(400).send('Email incrreto'); //retorno um erro
+                return res.status(400).send('Email ou Senha incorretos'); //retorno um erro
             };
 
-            if (user.password !== password)
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch)
             {
-                return res.status(400).send('Senha incorreta')
+                return res.status(400).send('Email ou Senha incorretos')
             };
 
             res.send('Login bem-sucedido');
--- backend/user.js
+++ backend/user.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: true,
-        unique: true,
+        required: true, //obrigatório
+        unique: true, //permite só um email com o mesmo nome
     },
     password: {
         type: String,
-        required: true,
+        required: true, //obrigatório
     },
 });
 
