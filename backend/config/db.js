4b1e552a801894b72c58fefa5acf130920d7f1f4
Hiago <hiago.ribeiro105@gmail.com>
2025-09-15 19:52:16 -0300

    update vercel 5

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,42 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
-}
+if (!cached) cached = global.mongoose = { conn: null };
 
 async function connectDB(uri) {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(uri).then((mongoose) => {
-      return mongoose;
-    });
-  }
-
-  cached.conn = await cached.promise;
+  if (cached.conn) return cached.conn;
+  const opts = { bufferCommands: false };
+  cached.conn = await mongoose.connect(uri, opts);
   return cached.conn;
 }
 
 module.exports = connectDB;
-
-
-
-
-// const connectDB = async () => {
-//   try 
-//   {
-//     await mongoose.connect(process.env.MONGO_URI);
-//     console.log('Conectado ao MongoDB');
-//   } 
-//   catch (err) 
-//   {
-//     console.error('Erro ao conectar:', err);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
