5f4226b948edb0e162ce0081ef9531ed3d401ddf
Hiago <hiago.ribeiro105@gmail.com>
2025-09-15 19:42:27 -0300

    update vercel 3

--- backend/Controllers/authControllers.js
+++ backend/Controllers/authControllers.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
-const connectDB = require('../db');
+const connectDB = require('../config/db');
 
 exports.signup = async (req, res) => {
   try {
